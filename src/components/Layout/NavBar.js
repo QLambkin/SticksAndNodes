d6f814dddb3c559383f34cf535ae196c579e4336
Quentin <q.lambkin@gmail.com>
2023-03-01 17:41:25 -0500

    Update title in navbar

--- src/components/Layout/NavBar.js
+++ src/components/Layout/NavBar.js
@@ -12,7 +12,7 @@ const NavBar = () => {
       <Container>
         <Navbar.Brand as={Link} to="/">
           <img className="logo" src={LogoQ} alt="Logo" />
-          UENTIN AND matt's GET RICH QUICK SCHEME
+          uentin presents: Sticks and Nodes
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
